044bbf28c84f231dc41d21d4c4cef9e317ecde14
Adam Dickmeiss <adam@indexdata.dk>
2025-01-29 13:09:38 +0100

    More testing

--- illmock/app/app_test.go
+++ illmock/app/app_test.go
@@ -3,8 +3,11 @@ package app
 import (
 	"errors"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWillSupplyLoaned(t *testing.T) {
@@ -21,4 +24,33 @@ func TestWillSupplyLoaned(t *testing.T) {
 	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		t.Fatalf("app.Run error %s", err.Error())
 	}
+}
+
+func TestBadMethod(t *testing.T) {
+	var app MockApp
+	go func() {
+		app.Run()
+	}()
+	time.Sleep(5 * time.Millisecond) // wait for app to serve
+
+	resp, err := http.Get("http://localhost:8081/iso18626")
+	assert.Nil(t, err)
+	assert.Equal(t, 405, resp.StatusCode)
+
+	resp, err = http.Post("http://localhost:8081/iso18626", "text/plain", strings.NewReader("hello"))
+	assert.Nil(t, err)
+	assert.Equal(t, 415, resp.StatusCode)
+
+	resp, err = http.Post("http://localhost:8081/iso18626", "text/xml", strings.NewReader("<badxml"))
+	assert.Nil(t, err)
+	assert.Equal(t, 400, resp.StatusCode)
+
+	resp, err = http.Post("http://localhost:8081/iso18626", "text/xml", strings.NewReader(
+		`<ISO18626Message ill:version="1.2">
+		<requestingAgencyMessageConfirmation/></ISO18626Message>`))
+	assert.Nil(t, err)
+	assert.Equal(t, 400, resp.StatusCode)
+
+	err = app.Shutdown()
+	assert.Nil(t, err)
 }
