a31cf94ed41117278e61a44f374b477a22bb3b70
Adam Dickmeiss <adam@indexdata.dk>
2025-01-30 11:03:07 +0100

    Comment on race condition

--- illmock/app/app_test.go
+++ illmock/app/app_test.go
@@ -35,10 +35,13 @@ func getFreePort() (int, error) {
 	if err != nil {
 		return 0, err
 	}
+	// release for now so it can be bound by the actual server
+	// a more robust solution would be to bind the server to the port and close it here
 	defer l.Close()
 	return l.Addr().(*net.TCPAddr).Port, nil
 }
 
+// getFreePortTest returns a free port as a string for testing.
 func getFreePortTest(t *testing.T) string {
 	port, err := getFreePort()
 	if err != nil {
