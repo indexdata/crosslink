a8ea03883154b872d9e253dfd82e1e97b1b343b0
Adam Dickmeiss <adam@indexdata.dk>
2025-02-07 16:58:39 +0100

    Larger response buffer in writeHttpResponse test

--- illmock/app/app_test.go
+++ illmock/app/app_test.go
@@ -165,16 +165,16 @@ func TestWriteResponseNil(t *testing.T) {
 
 func TestWriteHttpResponseWriteFailed(t *testing.T) {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		writeHttpResponse(w, []byte(strings.Repeat("S1", 16000)))
+		writeHttpResponse(w, []byte(strings.Repeat("S1", 160000)))
 	})
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
 	conn, err := net.Dial("tcp", server.URL[7:])
 	assert.Nil(t, err)
-	defer conn.Close()
 	n, err := conn.Write([]byte("POST / HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/xml\r\n" +
 		"Content-Length: 0\r\n\r\n"))
+	conn.Close()
 	assert.Nil(t, err)
 	assert.Greater(t, n, 20)
 }
