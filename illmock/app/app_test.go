7aa95e9604fbf205cad4ba1b231b8c6de2ae3852
Adam Dickmeiss <adam@indexdata.dk>
2025-02-27 19:06:23 +0100

    100% coverage

--- illmock/app/app_test.go
+++ illmock/app/app_test.go
@@ -738,3 +738,17 @@ func TestSendSupplyingAgencyUnexpectedISO18626message(t *testing.T) {
 	app.supplier.store(header, supplierInfo)
 	app.sendSupplyingAgencyMessage(header)
 }
+
+func TestIncomingPatronRequest(t *testing.T) {
+	var app MockApp
+	app.flowsApi = flows.CreateFlowsApi()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		msg := createPatronRequest()
+		app.incomingPatronRequest([]byte("<"), w, msg.Request)
+	})
+	server := httptest.NewServer(handler)
+	defer server.Close()
+	resp, err := http.Get(server.URL)
+	assert.Nil(t, err)
+	assert.Equal(t, 400, resp.StatusCode)
+}
