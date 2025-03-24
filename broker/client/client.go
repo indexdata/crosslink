66c9fc01ac36ab7ee1ecee2a45ef138384f626b4
Jakub Skoczen <jakub@indexdata.com>
2025-03-24 07:35:16 +0100

    Forward only first WillSupply

--- broker/client/client.go
+++ broker/client/client.go
@@ -77,7 +77,6 @@ func (c *Iso18626Client) createAndSendSupplyingAgencyMessage(ctx extctx.Extended
 		ctx.Logger().Error("failed to read ILL transaction", "error", err)
 		return events.EventStatusError, nil
 	}
-
 	resData := events.EventResult{}
 
 	var message = &iso18626.ISO18626Message{}
@@ -86,9 +85,15 @@ func (c *Iso18626Client) createAndSendSupplyingAgencyMessage(ctx extctx.Extended
 	if locSupplier == nil {
 		dStatus := iso18626.TypeStatusUnfilled
 		defaultStatus = &dStatus
+	} else if locSupplier.LastStatus.String == string(iso18626.TypeStatusWillSupply) {
+		fwStatus := illTrans.LastSupplierStatus.String
+		if len(fwStatus) > 0 && fwStatus != string(iso18626.TypeStatusExpectToSupply) {
+			resData.CustomData["note"] = "status WillSupply already communicated and will be ignored"
+			return events.EventStatusSuccess, &resData
+		}
 	}
-	statusInfo, statusErr := c.createStatusInfo(illTrans, locSupplier, defaultStatus)
 
+	statusInfo, statusErr := c.createStatusInfo(illTrans, locSupplier, defaultStatus)
 	message.SupplyingAgencyMessage = &iso18626.SupplyingAgencyMessage{
 		Header:      c.createMessageHeader(illTrans, peer, false),
 		MessageInfo: c.createMessageInfo(),
