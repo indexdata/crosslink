d203b113f649ddad31825a31cd6857d2c0ed4cf6
JanisSaldabols <143704574+JanisSaldabols@users.noreply.github.com>
2025-03-28 12:16:26 +0200

    CROSSLINK-92 Add reason and supplier request id to located supplier (#141)
    * CROSSLINK-92 Add reason and supplier request id to located supplier

--- broker/migrations/001_create_tables.up.sql
+++ broker/migrations/001_create_tables.up.sql
@@ -9,7 +9,7 @@ CREATE TABLE peer
     loans_count    INTEGER   NOT NULL DEFAULT 0,
     borrows_count  INTEGER   NOT NULL DEFAULT 0,
     vendor         VARCHAR   NOT NULL,
-    UNIQUE(symbol)
+    UNIQUE (symbol)
 );
 
 CREATE TABLE ill_transaction
@@ -74,16 +74,19 @@ CREATE TABLE event
 
 CREATE TABLE located_supplier
 (
-    id                 VARCHAR PRIMARY KEY,
-    ill_transaction_id VARCHAR NOT NULL,
-    supplier_id        VARCHAR NOT NULL,
-    ordinal            INT     NOT NULL DEFAULT 0,
-    supplier_status    VARCHAR,
-    prev_action        VARCHAR,
-    prev_status        VARCHAR,
-    last_action        VARCHAR,
-    last_status        VARCHAR,
-    local_id           VARCHAR,
+    id                  VARCHAR PRIMARY KEY,
+    ill_transaction_id  VARCHAR NOT NULL,
+    supplier_id         VARCHAR NOT NULL,
+    ordinal             INT     NOT NULL DEFAULT 0,
+    supplier_status     VARCHAR,
+    prev_action         VARCHAR,
+    prev_status         VARCHAR,
+    last_action         VARCHAR,
+    last_status         VARCHAR,
+    local_id            VARCHAR,
+    prev_reason         VARCHAR,
+    last_reason         VARCHAR,
+    supplier_request_id VARCHAR,
     FOREIGN KEY (ill_transaction_id) REFERENCES ill_transaction (id),
     FOREIGN KEY (supplier_id) REFERENCES peer (id)
 );
