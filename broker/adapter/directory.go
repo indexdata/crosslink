e87b28e6042c00269875a9ac28b6e12aa59b5123
JanisSaldabols <143704574+JanisSaldabols@users.noreply.github.com>
2025-04-11 17:36:23 +0300

    CROSSLINK-60 Add filtering and sorting (#160)

--- broker/adapter/directory.go
+++ broker/adapter/directory.go
@@ -1,7 +1,13 @@
 package adapter
 
+import (
+	extctx "github.com/indexdata/crosslink/broker/common"
+	"github.com/indexdata/crosslink/iso18626"
+)
+
 type DirectoryLookupAdapter interface {
 	Lookup(params DirectoryLookupParams) ([]DirectoryEntry, error)
+	FilterAndSort(ctx extctx.ExtendedContext, entries []Supplier, requesterData map[string]any, serviceInfo *iso18626.ServiceInfo, billingInfo *iso18626.BillingInfo) []Supplier
 }
 
 type DirectoryLookupParams struct {
@@ -14,4 +20,14 @@ type DirectoryEntry struct {
 	URL        string
 	Vendor     string
 	CustomData map[string]any
+}
+
+type Supplier struct {
+	LocalIdentifier string
+	PeerId          string
+	CustomData      map[string]any
+	Ratio           float32
+	Symbol          string
+	NetworkPriority float64
+	Cost            float64
 }
