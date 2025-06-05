2120084eeedc67c37be56b52850cd0a061e60361
Adam Dickmeiss <adam@indexdata.dk>
2025-06-05 17:45:09 +0200

    Fix last_signal migration
    Did not set a default value.

--- broker/migrations/008_last_signal_in_event.up.sql
+++ broker/migrations/008_last_signal_in_event.up.sql
@@ -1,1 +1,5 @@
-ALTER TABLE event ADD COLUMN last_signal VARCHAR NOT NULL;
+ALTER TABLE event ADD COLUMN last_signal VARCHAR DEFAULT '';
+
+UPDATE event SET last_signal = '';
+
+ALTER TABLE event ALTER COLUMN last_signal SET NOT NULL;
