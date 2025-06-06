dc562b3971ea17394ad833b1834a341103ccedb7
Adam Dickmeiss <adam@indexdata.dk>
2025-06-06 10:08:41 +0200

    CROSSLINK-141 last_signal migration cleanup (#228)

--- broker/migrations/008_last_signal_in_event.up.sql
+++ broker/migrations/008_last_signal_in_event.up.sql
@@ -1,5 +1,1 @@
-ALTER TABLE event ADD COLUMN last_signal VARCHAR DEFAULT '';
-
-UPDATE event SET last_signal = '';
-
-ALTER TABLE event ALTER COLUMN last_signal SET NOT NULL;
+ALTER TABLE event ADD COLUMN last_signal VARCHAR DEFAULT '' NOT NULL;
