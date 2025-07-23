02e4a5e92e33d432961e794a088ae654e67139bc
Jakub Skoczen <jakub@indexdata.com>
2025-07-23 12:46:55 +0200

    Refactor error logging in the client

--- broker/events/logging.go
+++ broker/events/logging.go
@@ -4,7 +4,24 @@ import extctx "github.com/indexdata/crosslink/broker/common"
 
 func LogErrorAndReturnResult(ctx extctx.ExtendedContext, component string, message string, err error) (EventStatus, *EventResult) {
 	ctx.Logger().Error(message, "error", err, "component", component)
-	return NewErrorResult(message, err.Error())
+	cause := ""
+	if err != nil {
+		cause = err.Error()
+	}
+	return NewErrorResult(message, cause)
+}
+
+func LogErrorAndReturnExistingResult(ctx extctx.ExtendedContext, component string, message string, err error, existingResult *EventResult) (EventStatus, *EventResult) {
+	ctx.Logger().Error(message, "error", err, "component", component)
+	cause := ""
+	if err != nil {
+		cause = err.Error()
+	}
+	existingResult.EventError = &EventError{
+		Message: message,
+		Cause:   cause,
+	}
+	return EventStatusError, existingResult
 }
 
 func LogProblemAndReturnResult(ctx extctx.ExtendedContext, component string, problem string, message string, customResult map[string]any) (EventStatus, *EventResult) {
