05792c0ace399d1d70dec93c295b42e1aff563ef
Jakub Skoczen <jakub@indexdata.com>
2025-08-08 01:47:32 +0200

    Confirm requster msg based on on the outgoing message
    not the transaction or supplier status

--- broker/service/workflow.go
+++ broker/service/workflow.go
@@ -111,12 +111,8 @@ func (w *WorkflowManager) RequesterMessageReceived(ctx extctx.ExtendedContext, e
 
 func (w *WorkflowManager) OnMessageSupplierComplete(ctx extctx.ExtendedContext, event events.Event) {
 	ctx = ctx.WithArgs(ctx.LoggerArgs().WithComponent(WF_COMP))
-	illTrans, err := w.illRepo.GetIllTransactionById(ctx, event.IllTransactionID)
-	if err != nil {
-		ctx.Logger().Error("failed to read ILL transaction", "error", err)
-		return
-	}
-	if illTrans.LastRequesterAction.String != ill_db.RequestAction {
+	if event.ResultData.OutgoingMessage != nil && event.ResultData.OutgoingMessage.Request == nil {
+		//message was not a request so we need to confirm it
 		extctx.Must(ctx, func() (string, error) {
 			return w.eventBus.CreateTaskBroadcast(event.IllTransactionID, events.EventNameConfirmRequesterMsg, events.EventData{}, &event.ID)
 		}, "")
