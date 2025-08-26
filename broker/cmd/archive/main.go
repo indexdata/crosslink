da4c9163cd22b5266f9f588673e82d77f02e19f5
Adam Dickmeiss <adam@indexdata.dk>
2025-08-26 16:56:22 +0200

    archive arguments

--- broker/cmd/archive/main.go
+++ broker/cmd/archive/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 
 	"fmt"
 	"os"
@@ -10,10 +11,15 @@ import (
 )
 
 func main() {
+	var statusList string
+	var duration string
+	flag.StringVar(&statusList, "statuses", "LoanCompleted,CopyCompleted,Unfilled", "comma separated list of statuses to archive")
+	flag.StringVar(&duration, "duration", "5d", "archive transactions older than this duration, for example 10d")
+	flag.Parse()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// TODO parse command line arguments
-	err := app.Archive(ctx, "LoanCompleted,CopyCompleted,Unfilled", "5d")
+	err := app.Archive(ctx, statusList, duration)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
