66db02a2818a33c3248081261f1fda15ec157494
Adam Dickmeiss <adam@indexdata.dk>
2025-10-06 11:33:48 +0200

    testing with dateTime

---
+++ ncip/ncip_const.go
@@ -0,0 +1,3 @@
+package ncip
+
+const NCIP_V2_02_XSD = "http://www.niso.org/ncip/v2_02/imp1/xsd/ncip_v2_02.xsd"
--- ncip/ncip_gen_test.go
+++ ncip/ncip_gen_test.go
@@ -4,20 +4,25 @@ import (
 	"encoding/xml"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/indexdata/go-utils/utils"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMarshal(t *testing.T) {
 	// Just a stub to make sure the generated code compiles
+
+	sampleTime := time.Date(2025, 10, 6, 11, 14, 0, 592000000, time.UTC)
 	req := &NCIPMessage{
-		Version: "2.0",
-		LookupUser: &LookupUser{
+		Version: NCIP_V2_02_XSD,
+		AcceptItem: &AcceptItem{
 			InitiationHeader: &InitiationHeader{},
+			DateForReturn:    &utils.XSDDateTime{Time: sampleTime},
 		},
 	}
 	var err error
-	exp, err := os.ReadFile("testdata/lookup_user_request.xml")
+	exp, err := os.ReadFile("testdata/ncip_sample.xml")
 	assert.NoError(t, err)
 	got, err := xml.MarshalIndent(req, "", "  ")
 	assert.NoError(t, err)
