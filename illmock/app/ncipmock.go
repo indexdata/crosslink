aacd7b34229fee54addde801a348e317016d644b
Adam Dickmeiss <adam@indexdata.dk>
2025-10-07 11:34:43 +0200

    Testing

--- illmock/app/ncipmock.go
+++ illmock/app/ncipmock.go
@@ -3,27 +3,30 @@ package app
 import (
 	"encoding/xml"
 	"io"
+	"mime"
 	"net/http"
 
 	"github.com/indexdata/crosslink/illmock/netutil"
 	"github.com/indexdata/crosslink/ncip"
 )
 
-func addProblem(problems []ncip.Problem, msg ncip.ProblemTypeMessage, detail string) []ncip.Problem {
+func setProblem(problem []ncip.Problem, msg ncip.ProblemTypeMessage, detail string) []ncip.Problem {
 	// only first problem reported
-	if len(problems) > 0 {
-		return problems
+	if problem != nil {
+		return problem
 	}
-	return append(problems, ncip.Problem{
-		ProblemType:   ncip.SchemeValuePair{Text: string(msg)},
-		ProblemDetail: detail,
-	})
+	return []ncip.Problem{
+		{
+			ProblemType:   ncip.SchemeValuePair{Text: string(msg)},
+			ProblemDetail: detail,
+		},
+	}
 }
 
 func handleLookupUser(problems []ncip.Problem, req *ncip.NCIPMessage, res *ncip.NCIPMessage) {
 	res.Version = req.Version
 	if req.LookupUser.UserId == nil && len(req.LookupUser.AuthenticationInput) == 0 {
-		problems = addProblem(problems, ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
+		problems = setProblem(problems, ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
 	}
 	res.LookupUserResponse = &ncip.LookupUserResponse{}
 	res.LookupUserResponse.UserId = req.LookupUser.UserId
@@ -35,7 +38,13 @@ func ncipMockHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	if r.Header.Get("Content-Type") != "application/xml" {
+	contentType := r.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnsupportedMediaType)
+		return
+	}
+	if mediaType != "application/xml" {
 		http.Error(w, "unsupported media type", http.StatusUnsupportedMediaType)
 		return
 	}
@@ -45,13 +54,13 @@ func ncipMockHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var ncipRequest ncip.NCIPMessage
-	var problems []ncip.Problem
+	var problem []ncip.Problem
 	err = xml.Unmarshal(byteReq, &ncipRequest)
 	if err != nil {
-		problems = addProblem(problems, ncip.InvalidMessageSyntaxError, err.Error())
+		problem = setProblem(problem, ncip.InvalidMessageSyntaxError, err.Error())
 	}
 	if ncipRequest.Version == "" {
-		problems = addProblem(problems, ncip.MissingVersion, "")
+		problem = setProblem(problem, ncip.MissingVersion, "")
 	}
 	var ncipResponse = ncip.NCIPMessage{
 		Version: ncipRequest.Version,
@@ -67,10 +76,10 @@ func ncipMockHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch {
 	case ncipRequest.LookupUser != nil:
-		handleLookupUser(problems, &ncipRequest, &ncipResponse)
+		handleLookupUser(problem, &ncipRequest, &ncipResponse)
 	default:
-		problems = addProblem(problems, ncip.UnsupportedService, "")
-		ncipResponse.Problem = problems
+		problem = setProblem(problem, ncip.UnsupportedService, "")
+		ncipResponse.Problem = problem
 	}
 	bytesResponse, err := xml.MarshalIndent(ncipResponse, "", "  ")
 	if err != nil {
@@ -79,8 +88,4 @@ func ncipMockHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/xml")
 	netutil.WriteHttpResponse(w, bytesResponse)
-}
-
-func getNcipMockHandler() http.HandlerFunc {
-	return ncipMockHandler
 }
---
+++ illmock/app/ncipmock_test.go
@@ -0,0 +1,138 @@
+package app
+
+import (
+	"bytes"
+	"encoding/xml"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/indexdata/crosslink/ncip"
+	"github.com/stretchr/testify/assert"
+)
+
+var server *httptest.Server
+
+func TestMain(m *testing.M) {
+	server = httptest.NewServer(http.HandlerFunc(ncipMockHandler))
+	defer server.Close()
+	m.Run()
+}
+
+func TestGet(t *testing.T) {
+	resp, err := http.Get(server.URL)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusMethodNotAllowed, resp.StatusCode)
+}
+
+func TestPostNoMediaType(t *testing.T) {
+	resp, err := http.Post(server.URL, "", nil)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusUnsupportedMediaType, resp.StatusCode)
+	defer func() {
+		dErr := resp.Body.Close()
+		assert.NoError(t, dErr)
+	}()
+	buf, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	assert.Contains(t, string(buf), "mime: no media type")
+}
+
+func TestPostTextPlain(t *testing.T) {
+	resp, err := http.Post(server.URL, "text/plain", nil)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusUnsupportedMediaType, resp.StatusCode)
+	defer func() {
+		dErr := resp.Body.Close()
+		assert.NoError(t, dErr)
+	}()
+	buf, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	assert.Contains(t, string(buf), "unsupported media type")
+}
+
+func TestPostXml(t *testing.T) {
+	resp, err := http.Post(server.URL, "application/xml;charset=utf-8", nil)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	defer func() {
+		dErr := resp.Body.Close()
+		assert.NoError(t, dErr)
+	}()
+	buf, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	var ncipResponse ncip.NCIPMessage
+	err = xml.Unmarshal(buf, &ncipResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, ncipResponse.Problem)
+	assert.Len(t, ncipResponse.Problem, 1)
+	assert.Equal(t, string(ncip.InvalidMessageSyntaxError), ncipResponse.Problem[0].ProblemType.Text)
+}
+
+func TestPostMissingVersion(t *testing.T) {
+	req := ncip.NCIPMessage{}
+	buf, err := xml.Marshal(req)
+	assert.NoError(t, err)
+	resp, err := http.Post(server.URL, "application/xml", bytes.NewReader(buf))
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	defer func() {
+		dErr := resp.Body.Close()
+		assert.NoError(t, dErr)
+	}()
+	buf, err = io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	var ncipResponse ncip.NCIPMessage
+	err = xml.Unmarshal(buf, &ncipResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, ncipResponse.Problem)
+	assert.Len(t, ncipResponse.Problem, 1)
+	assert.Equal(t, string(ncip.MissingVersion), ncipResponse.Problem[0].ProblemType.Text)
+}
+
+func TestPostMissingMessageType(t *testing.T) {
+	req := ncip.NCIPMessage{Version: ncip.NCIP_V2_02_XSD}
+	buf, err := xml.Marshal(req)
+	assert.NoError(t, err)
+	resp, err := http.Post(server.URL, "application/xml", bytes.NewReader(buf))
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	defer func() {
+		dErr := resp.Body.Close()
+		assert.NoError(t, dErr)
+	}()
+	buf, err = io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	var ncipResponse ncip.NCIPMessage
+	err = xml.Unmarshal(buf, &ncipResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, ncipResponse.Problem)
+	assert.Len(t, ncipResponse.Problem, 1)
+	assert.Equal(t, string(ncip.UnsupportedService), ncipResponse.Problem[0].ProblemType.Text)
+}
+
+func TestPostLookupUserMissingId(t *testing.T) {
+	req := ncip.NCIPMessage{
+		Version:    ncip.NCIP_V2_02_XSD,
+		LookupUser: &ncip.LookupUser{},
+	}
+	buf, err := xml.Marshal(req)
+	assert.NoError(t, err)
+	resp, err := http.Post(server.URL, "application/xml", bytes.NewReader(buf))
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	defer func() {
+		dErr := resp.Body.Close()
+		assert.NoError(t, dErr)
+	}()
+	buf, err = io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	var ncipResponse ncip.NCIPMessage
+	err = xml.Unmarshal(buf, &ncipResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, ncipResponse.LookupUserResponse)
+	assert.Len(t, ncipResponse.LookupUserResponse.Problem, 1)
+	assert.Equal(t, string(ncip.NeededDataMissing), ncipResponse.LookupUserResponse.Problem[0].ProblemType.Text)
+	assert.Equal(t, "UserId or AuthenticationInput is required", ncipResponse.LookupUserResponse.Problem[0].ProblemDetail)
+}
