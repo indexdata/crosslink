b1bff0328f8b2259b6db2b870bd4a313f055dbd5
Adam Dickmeiss <adam@indexdata.dk>
2025-10-07 22:35:46 +0200

    Redundant problem check

--- illmock/app/ncipmock.go
+++ illmock/app/ncipmock.go
@@ -22,12 +22,11 @@ func setProblem(msg ncip.ProblemTypeMessage, detail string) []ncip.Problem {
 
 func handleLookupUser(req *ncip.NCIPMessage, res *ncip.NCIPMessage) {
 	var problem []ncip.Problem
-	if req.LookupUser.UserId == nil && len(req.LookupUser.AuthenticationInput) == 0 {
-		problem = setProblem(ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
-	}
 	res.LookupUserResponse = &ncip.LookupUserResponse{}
 	res.LookupUserResponse.UserId = req.LookupUser.UserId
-	if problem == nil && req.LookupUser.UserId != nil && strings.HasPrefix(req.LookupUser.UserId.UserIdentifierValue, "f") {
+	if req.LookupUser.UserId == nil && len(req.LookupUser.AuthenticationInput) == 0 {
+		problem = setProblem(ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
+	} else if req.LookupUser.UserId != nil && strings.HasPrefix(req.LookupUser.UserId.UserIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownUser, req.LookupUser.UserId.UserIdentifierValue)
 	}
 	res.LookupUserResponse.Problem = problem
@@ -38,11 +37,9 @@ func handleAcceptItem(req *ncip.NCIPMessage, res *ncip.NCIPMessage) {
 	res.AcceptItemResponse = &ncip.AcceptItemResponse{}
 	if req.AcceptItem.RequestId.RequestIdentifierValue == "" {
 		problem = setProblem(ncip.NeededDataMissing, "RequestId is required")
-	}
-	if problem == nil && req.AcceptItem.UserId != nil && strings.HasPrefix(req.AcceptItem.UserId.UserIdentifierValue, "f") {
+	} else if req.AcceptItem.UserId != nil && strings.HasPrefix(req.AcceptItem.UserId.UserIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownUser, req.AcceptItem.UserId.UserIdentifierValue)
-	}
-	if problem == nil && req.AcceptItem.ItemId != nil && strings.HasPrefix(req.AcceptItem.ItemId.ItemIdentifierValue, "f") {
+	} else if req.AcceptItem.ItemId != nil && strings.HasPrefix(req.AcceptItem.ItemId.ItemIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownItem, req.AcceptItem.ItemId.ItemIdentifierValue)
 	}
 	res.AcceptItemResponse.RequestId = &req.AcceptItem.RequestId
@@ -65,20 +62,15 @@ func handleRequestItem(req *ncip.NCIPMessage, res *ncip.NCIPMessage) {
 	res.RequestItemResponse = &ncip.RequestItemResponse{}
 	if req.RequestItem.UserId == nil && len(req.RequestItem.AuthenticationInput) == 0 {
 		problem = setProblem(ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
-	}
-	if len(req.RequestItem.BibliographicId) == 0 && len(req.RequestItem.ItemId) == 0 {
+	} else if len(req.RequestItem.BibliographicId) == 0 && len(req.RequestItem.ItemId) == 0 {
 		problem = setProblem(ncip.NeededDataMissing, "BibliographicId or ItemId is required")
-	}
-	if problem == nil && req.RequestItem.RequestType.Text == "" {
+	} else if req.RequestItem.RequestType.Text == "" {
 		problem = setProblem(ncip.NeededDataMissing, "RequestType is required")
-	}
-	if problem == nil && req.RequestItem.RequestScopeType.Text == "" {
+	} else if req.RequestItem.RequestScopeType.Text == "" {
 		problem = setProblem(ncip.NeededDataMissing, "RequestScopeType is required")
-	}
-	if problem == nil && req.RequestItem.UserId != nil && strings.HasPrefix(req.RequestItem.UserId.UserIdentifierValue, "f") {
+	} else if req.RequestItem.UserId != nil && strings.HasPrefix(req.RequestItem.UserId.UserIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownUser, req.RequestItem.UserId.UserIdentifierValue)
-	}
-	if problem == nil && len(req.RequestItem.ItemId) > 0 && strings.HasPrefix(req.RequestItem.ItemId[0].ItemIdentifierValue, "f") {
+	} else if len(req.RequestItem.ItemId) > 0 && strings.HasPrefix(req.RequestItem.ItemId[0].ItemIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownItem, req.RequestItem.ItemId[0].ItemIdentifierValue)
 	}
 	if len(req.RequestItem.ItemId) > 0 {
@@ -96,17 +88,13 @@ func handleCancelRequestItem(req *ncip.NCIPMessage, res *ncip.NCIPMessage) {
 	res.CancelRequestItemResponse = &ncip.CancelRequestItemResponse{}
 	if req.CancelRequestItem.UserId == nil && len(req.CancelRequestItem.AuthenticationInput) == 0 {
 		problem = setProblem(ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
-	}
-	if problem == nil && req.CancelRequestItem.RequestId == nil && req.CancelRequestItem.ItemId == nil {
+	} else if req.CancelRequestItem.RequestId == nil && req.CancelRequestItem.ItemId == nil {
 		problem = setProblem(ncip.NeededDataMissing, "RequestId or ItemId is required")
-	}
-	if problem == nil && req.CancelRequestItem.RequestType.Text == "" {
+	} else if req.CancelRequestItem.RequestType.Text == "" {
 		problem = setProblem(ncip.NeededDataMissing, "RequestType is required")
-	}
-	if problem == nil && req.CancelRequestItem.UserId != nil && strings.HasPrefix(req.CancelRequestItem.UserId.UserIdentifierValue, "f") {
+	} else if req.CancelRequestItem.UserId != nil && strings.HasPrefix(req.CancelRequestItem.UserId.UserIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownUser, req.CancelRequestItem.UserId.UserIdentifierValue)
-	}
-	if problem == nil && req.CancelRequestItem.ItemId != nil && strings.HasPrefix(req.CancelRequestItem.ItemId.ItemIdentifierValue, "f") {
+	} else if req.CancelRequestItem.ItemId != nil && strings.HasPrefix(req.CancelRequestItem.ItemId.ItemIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownItem, req.CancelRequestItem.ItemId.ItemIdentifierValue)
 	}
 	res.CancelRequestItemResponse.ItemId = req.CancelRequestItem.ItemId
@@ -120,8 +108,7 @@ func handleCheckInItem(req *ncip.NCIPMessage, res *ncip.NCIPMessage) {
 	res.CheckInItemResponse = &ncip.CheckInItemResponse{}
 	if req.CheckInItem.ItemId.ItemIdentifierValue == "" {
 		problem = setProblem(ncip.NeededDataMissing, "ItemId is required")
-	}
-	if problem == nil && strings.HasPrefix(req.CheckInItem.ItemId.ItemIdentifierValue, "f") {
+	} else if strings.HasPrefix(req.CheckInItem.ItemId.ItemIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownItem, req.CheckInItem.ItemId.ItemIdentifierValue)
 	}
 	res.CheckInItemResponse.ItemId = &req.CheckInItem.ItemId
@@ -133,14 +120,11 @@ func handleCheckOutItem(req *ncip.NCIPMessage, res *ncip.NCIPMessage) {
 	res.CheckOutItemResponse = &ncip.CheckOutItemResponse{}
 	if req.CheckOutItem.UserId == nil && len(req.CheckOutItem.AuthenticationInput) == 0 {
 		problem = setProblem(ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
-	}
-	if req.CheckOutItem.ItemId.ItemIdentifierValue == "" {
+	} else if req.CheckOutItem.ItemId.ItemIdentifierValue == "" {
 		problem = setProblem(ncip.NeededDataMissing, "ItemId is required")
-	}
-	if problem == nil && req.CheckOutItem.UserId != nil && strings.HasPrefix(req.CheckOutItem.UserId.UserIdentifierValue, "f") {
+	} else if req.CheckOutItem.UserId != nil && strings.HasPrefix(req.CheckOutItem.UserId.UserIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownUser, req.CheckOutItem.UserId.UserIdentifierValue)
-	}
-	if problem == nil && strings.HasPrefix(req.CheckOutItem.ItemId.ItemIdentifierValue, "f") {
+	} else if strings.HasPrefix(req.CheckOutItem.ItemId.ItemIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownItem, req.CheckOutItem.ItemId.ItemIdentifierValue)
 	}
 	res.CheckOutItemResponse.ItemId = &req.CheckOutItem.ItemId
@@ -153,11 +137,9 @@ func handleCreateUserFiscalTransaction(req *ncip.NCIPMessage, res *ncip.NCIPMess
 	res.CreateUserFiscalTransactionResponse = &ncip.CreateUserFiscalTransactionResponse{}
 	if req.CreateUserFiscalTransaction.UserId == nil && len(req.CreateUserFiscalTransaction.AuthenticationInput) == 0 {
 		problem = setProblem(ncip.NeededDataMissing, "UserId or AuthenticationInput is required")
-	}
-	if problem == nil && req.CreateUserFiscalTransaction.FiscalTransactionInformation.FiscalActionType.Text == "" {
+	} else if req.CreateUserFiscalTransaction.FiscalTransactionInformation.FiscalActionType.Text == "" {
 		problem = setProblem(ncip.NeededDataMissing, "FiscalTransactionInformation is required")
-	}
-	if problem == nil && req.CreateUserFiscalTransaction.UserId != nil && strings.HasPrefix(req.CreateUserFiscalTransaction.UserId.UserIdentifierValue, "f") {
+	} else if req.CreateUserFiscalTransaction.UserId != nil && strings.HasPrefix(req.CreateUserFiscalTransaction.UserId.UserIdentifierValue, "f") {
 		problem = setProblem(ncip.UnknownUser, req.CreateUserFiscalTransaction.UserId.UserIdentifierValue)
 	}
 	res.CreateUserFiscalTransactionResponse.UserId = req.CreateUserFiscalTransaction.UserId
