22489e6c78284acf29c27b57ceac8de3a821a04b
Adam Dickmeiss <adam@indexdata.dk>
2026-02-18 13:34:29 +0100

    CrossLink

--- broker/common/const.go
+++ broker/common/const.go
@@ -12,7 +12,7 @@ type Vendor string
 const (
 	VendorAlma      Vendor = "Alma"
 	VendorReShare   Vendor = "ReShare"
-	VendorCrosslink Vendor = "Crosslink"
+	VendorCrosslink Vendor = "CrossLink"
 	VendorUnknown   Vendor = "Unknown"
 )
 
