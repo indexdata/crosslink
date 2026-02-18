fab0c45e53b432f5d99a46b9533b5f27fbc9657e
JanisSaldabols <143704574+JanisSaldabols@users.noreply.github.com>
2026-02-18 16:39:25 +0200

    CROSSLINK-213 Add data model for items and notifications (#403)

--- broker/migrations/018_add_items.up.sql
+++ broker/migrations/018_add_items.up.sql
@@ -1,24 +1,3 @@
-CREATE TABLE patron_request
-(
-    id                VARCHAR PRIMARY KEY,
-    timestamp         TIMESTAMP NOT NULL DEFAULT now(),
-    ill_request       jsonb,
-    state             VARCHAR   NOT NULL,
-    side              VARCHAR   NOT NULL,
-    patron            VARCHAR,
-    requester_symbol  VARCHAR,
-    supplier_symbol   VARCHAR,
-    tenant            VARCHAR,
-    requester_req_id  VARCHAR
-);
-
-CREATE OR REPLACE FUNCTION get_next_hrid(prefix VARCHAR) RETURNS VARCHAR AS $$
-BEGIN
-    EXECUTE format('CREATE SEQUENCE IF NOT EXISTS %I START 1', LOWER(prefix) || '_hrid_seq');
-    RETURN UPPER(prefix) || '-' || nextval(LOWER(prefix) || '_hrid_seq')::TEXT;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE TABLE item
 (
     id          VARCHAR PRIMARY KEY,
--- broker/sqlc/pr_query.sql
+++ broker/sqlc/pr_query.sql
@@ -39,3 +39,64 @@ LIMIT 1;
 
 -- name: GetNextHrid :one
 SELECT get_next_hrid($1)::TEXT AS hrid;
+
+-- name: SaveItem :one
+INSERT INTO item (id, pr_id, barcode, call_number, title, item_id, created_at)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+ON CONFLICT (id) DO UPDATE
+    SET pr_id       = EXCLUDED.pr_id,
+        barcode = EXCLUDED.barcode,
+        call_number = EXCLUDED.call_number,
+        title = EXCLUDED.title,
+        item_id = EXCLUDED.item_id,
+        created_at = EXCLUDED.created_at
+RETURNING sqlc.embed(item);
+
+-- name: GetItemById :one
+SELECT sqlc.embed(item)
+FROM item
+WHERE id = $1
+LIMIT 1;
+
+-- name: GetItemsByPrId :many
+SELECT sqlc.embed(item)
+FROM item
+WHERE pr_id = $1;
+
+-- name: DeleteItemById :exec
+DELETE
+FROM item
+WHERE id = $1;
+
+-- name: SaveNotification :one
+INSERT INTO notification (id, pr_id, from_symbol, to_symbol, side, note, cost, currency, condition, receipt, created_at, acknowledged_at)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+ON CONFLICT (id) DO UPDATE
+    SET pr_id           = EXCLUDED.pr_id,
+        from_symbol     = EXCLUDED.from_symbol,
+        to_symbol       = EXCLUDED.to_symbol,
+        side            = EXCLUDED.side,
+        note            = EXCLUDED.note,
+        cost            = EXCLUDED.cost,
+        currency        = EXCLUDED.currency,
+        condition       = EXCLUDED.condition,
+        receipt         = EXCLUDED.receipt,
+        created_at      = EXCLUDED.created_at,
+        acknowledged_at = EXCLUDED.acknowledged_at
+RETURNING sqlc.embed(notification);
+
+-- name: GetNotificationById :one
+SELECT sqlc.embed(notification)
+FROM notification
+WHERE id = $1
+LIMIT 1;
+
+-- name: GetNotificationsByPrId :many
+SELECT sqlc.embed(notification)
+FROM notification
+WHERE pr_id = $1;
+
+-- name: DeleteNotificationById :exec
+DELETE
+FROM notification
+WHERE id = $1;
