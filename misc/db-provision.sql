8677fd50bdc7fe4c736e71d0c8713db43fbceb27
Jakub Skoczen <jakub@indexdata.com>
2026-02-21 11:11:08 +0100

    More prominent args

--- misc/db-provision.sql
+++ misc/db-provision.sql
@@ -2,7 +2,7 @@
 CREATE ROLE crosslink LOGIN PASSWORD 'REPLACE_WITH_SECRET';
 
 -- Allow this role to connect to the database
-GRANT CONNECT ON DATABASE db_name TO crosslink;
+GRANT CONNECT ON DATABASE 'DB_NAME' TO crosslink;
 
 -- Create schema owned by crosslink
 -- Note: if schema already exists (migration) ensure USAGE and CREATE are granted for future and existing objects
@@ -12,5 +12,5 @@ CREATE SCHEMA IF NOT EXISTS crosslink_broker AUTHORIZATION crosslink;
 REVOKE ALL ON SCHEMA public FROM crosslink;
 
 -- Set default schema for this role
-ALTER ROLE crosslink IN DATABASE db_name
+ALTER ROLE crosslink IN DATABASE 'DB_NAME'
 SET search_path = crosslink_broker, pg_temp;
