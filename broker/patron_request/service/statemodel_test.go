b8f931287bb98c6d05d620722d3bd566fe0c5702
Jakub Skoczen <jakub@indexdata.com>
2026-03-02 08:21:10 +0100

    Add validation test cases

--- broker/patron_request/service/statemodel_test.go
+++ broker/patron_request/service/statemodel_test.go
@@ -58,3 +58,106 @@ func TestValidateStateModelInvalidMessageEvent(t *testing.T) {
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "not a built-in message event")
 }
+
+func TestValidateStateModelUnsupportedSide(t *testing.T) {
+	model := &proapi.StateModel{
+		Type:    proapi.StateModelTypeStateModel,
+		Name:    "test",
+		Version: "1.0.0",
+		States: []proapi.ModelState{
+			{
+				Name: "NEW",
+				Side: proapi.ModelStateSide("UNKNOWN"),
+			},
+		},
+	}
+
+	err := ValidateStateModel(model)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "unsupported side")
+}
+
+func TestValidateStateModelInvalidActionSuccessTransitionTarget(t *testing.T) {
+	invalidTarget := "DOES_NOT_EXIST"
+	model := &proapi.StateModel{
+		Type:    proapi.StateModelTypeStateModel,
+		Name:    "test",
+		Version: "1.0.0",
+		States: []proapi.ModelState{
+			{
+				Name: "NEW",
+				Side: proapi.REQUESTER,
+				Actions: &[]proapi.ModelAction{
+					{
+						Name: string(BorrowerActionValidate),
+						Transitions: &struct {
+							Failure *string `json:"failure,omitempty"`
+							Success *string `json:"success,omitempty"`
+						}{
+							Success: &invalidTarget,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := ValidateStateModel(model)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "invalid success transition target")
+}
+
+func TestValidateStateModelInvalidActionFailureTransitionTarget(t *testing.T) {
+	invalidTarget := "DOES_NOT_EXIST"
+	model := &proapi.StateModel{
+		Type:    proapi.StateModelTypeStateModel,
+		Name:    "test",
+		Version: "1.0.0",
+		States: []proapi.ModelState{
+			{
+				Name: "VALIDATED",
+				Side: proapi.REQUESTER,
+				Actions: &[]proapi.ModelAction{
+					{
+						Name: string(BorrowerActionSendRequest),
+						Transitions: &struct {
+							Failure *string `json:"failure,omitempty"`
+							Success *string `json:"success,omitempty"`
+						}{
+							Failure: &invalidTarget,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := ValidateStateModel(model)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "invalid failure transition target")
+}
+
+func TestValidateStateModelInvalidEventTransitionTarget(t *testing.T) {
+	invalidTarget := "DOES_NOT_EXIST"
+	model := &proapi.StateModel{
+		Type:    proapi.StateModelTypeStateModel,
+		Name:    "test",
+		Version: "1.0.0",
+		States: []proapi.ModelState{
+			{
+				Name: "SENT",
+				Side: proapi.REQUESTER,
+				Events: &[]proapi.ModelEvent{
+					{
+						Name:       string(SupplierWillSupply),
+						Transition: &invalidTarget,
+					},
+				},
+			},
+		},
+	}
+
+	err := ValidateStateModel(model)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "invalid transition target")
+}
