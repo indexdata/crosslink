5e4e09019d17f4400dc54fecd3912a06a772dd58
Jakub Skoczen <jakub@indexdata.com>
2026-04-09 16:15:30 +0200

    Use ROOT_TAG const

--- iso18626/models_ns.go
+++ iso18626/models_ns.go
@@ -6,7 +6,7 @@ const IllV1_2 = "1.2"
 
 func NewISO18626Message() *ISO18626Message {
 	msg := ISO18626Message{}
-	msg.XMLName = xml.Name{Space: TARGET_NAMESPACE, Local: "ISO18626Message"}
+	msg.XMLName = xml.Name{Space: TARGET_NAMESPACE, Local: ROOT_TAG}
 	msg.Version = IllV1_2
 	return &msg
 }
