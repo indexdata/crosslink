4d3b117d6aa724060c9ed55ff9b5b62e50bd99b8
Adam Dickmeiss <adam@indexdata.dk>
2026-04-17 16:20:27 +0200

    Fix typo

--- broker/test/api/sse_broker_test.go
+++ broker/test/api/sse_broker_test.go
@@ -105,7 +105,7 @@ func TestSseEndpointNoSide(t *testing.T) {
 }
 
 func TestSseEndpointNoSymbol(t *testing.T) {
-	resp, err := http.Get(getLocalhostWithPort() + "/sse/events?side=borrowwing&other=/broker/")
+	resp, err := http.Get(getLocalhostWithPort() + "/sse/events?side=borrowing&other=/broker/")
 	assert.NoError(t, err)
 	bodyBytes, err := io.ReadAll(resp.Body)
 	assert.NoError(t, err)
