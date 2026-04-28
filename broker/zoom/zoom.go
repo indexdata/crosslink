f05a753d18c4cc0cc70a44660a05a043a692251c
Adam Dickmeiss <adam@indexdata.dk>
2026-04-28 16:31:23 +0200

    CROSSLINK-268 zoom wrapper

---
+++ broker/zoom/zoom.go
@@ -0,0 +1,128 @@
+package zoom
+
+/*
+#cgo LDFLAGS: -lyaz
+#include <yaz/zoom.h>
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"fmt"
+	"runtime"
+	"unsafe"
+)
+
+type Options map[string]string
+
+type Connection struct {
+	conn C.ZOOM_connection
+}
+
+type ResultSet struct {
+	connection *Connection
+	rs         C.ZOOM_resultset
+}
+
+type Record struct {
+	rec C.ZOOM_record
+}
+
+func NewConnection(options Options) *Connection {
+	c := &Connection{}
+	cOptions := options.ToZoomOptions()
+	defer C.ZOOM_options_destroy(cOptions)
+	c.conn = C.ZOOM_connection_create(cOptions)
+	runtime.SetFinalizer(c, func(c *Connection) {
+		if c.conn != nil {
+			C.ZOOM_connection_destroy(c.conn)
+			c.conn = nil
+		}
+	})
+	return c
+}
+
+func (c *Connection) Connect(host string) error {
+	cHost := C.CString(host)
+	defer C.free(unsafe.Pointer(cHost))
+	C.ZOOM_connection_connect(c.conn, cHost, 0)
+	code := C.ZOOM_connection_error(c.conn, nil, nil)
+	if code != 0 {
+		return fmt.Errorf("failed to connect: %s", C.GoString(C.ZOOM_connection_errmsg(c.conn)))
+	}
+	return nil
+}
+
+func (c *Connection) Close() {
+	if c.conn != nil {
+		C.ZOOM_connection_destroy(c.conn)
+		c.conn = nil
+	}
+}
+
+func (c *Connection) Search(query string) (*ResultSet, error) {
+	if c.conn == nil {
+		return nil, fmt.Errorf("connection is not established")
+	}
+	cQuery := C.CString(query)
+	defer C.free(unsafe.Pointer(cQuery))
+	cSet := C.ZOOM_connection_search_pqf(c.conn, cQuery)
+	set := &ResultSet{rs: cSet, connection: c}
+	runtime.SetFinalizer(set, func(set *ResultSet) {
+		if set.rs != nil {
+			C.ZOOM_resultset_destroy(set.rs)
+			set.rs = nil
+		}
+	})
+	code := C.ZOOM_connection_error(c.conn, nil, nil)
+	if code != 0 {
+		return nil, fmt.Errorf("search failed: %s", C.GoString(C.ZOOM_connection_errmsg(c.conn)))
+	}
+	return set, nil
+}
+
+func (o *Options) ToZoomOptions() C.ZOOM_options {
+	zo := C.ZOOM_options_create()
+	for key, value := range *o {
+		cKey := C.CString(key)
+		cValue := C.CString(value)
+		defer C.free(unsafe.Pointer(cKey))
+		defer C.free(unsafe.Pointer(cValue))
+		C.ZOOM_options_set(zo, cKey, cValue)
+	}
+	return zo
+}
+
+func (r *ResultSet) Count() int {
+	return int(C.ZOOM_resultset_size(r.rs))
+}
+
+func (r *ResultSet) GetRecord(index int) (*Record, error) {
+	cRecord := C.ZOOM_resultset_record(r.rs, C.size_t(index))
+	if cRecord == nil {
+		return nil, nil
+	}
+	record := &Record{rec: cRecord}
+	runtime.SetFinalizer(record, func(record *Record) {
+		if record.rec != nil {
+			C.ZOOM_record_destroy(record.rec)
+			record.rec = nil
+		}
+	})
+	code := C.ZOOM_connection_error(r.connection.conn, nil, nil)
+	if code != 0 {
+		return nil, fmt.Errorf("get record failed: %s", C.GoString(C.ZOOM_connection_errmsg(r.connection.conn)))
+	}
+	return record, nil
+}
+
+func (r *Record) Data(dataType string) string {
+	cType := C.CString(dataType)
+	defer C.free(unsafe.Pointer(cType))
+	cData := C.ZOOM_record_get(r.rec, cType, nil)
+	if cData == nil {
+		return ""
+	}
+	// the returned cData is owned by record and will be freed when the record is destroyed
+	data := C.GoString(cData)
+	return data
+}
---
+++ broker/zoom/zoom_test.go
@@ -0,0 +1,67 @@
+package zoom
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/indexdata/go-utils/utils"
+	"github.com/stretchr/testify/assert"
+
+	test "github.com/indexdata/crosslink/broker/test/utils"
+)
+
+func TestConnection(t *testing.T) {
+	options := Options{
+		"databaseName": "testdb",
+		"username":     "testuser",
+		"password":     "testpass",
+	}
+	conn := NewConnection(options)
+	assert.NotNil(t, conn)
+	conn.Close()
+}
+
+func TestConnect(t *testing.T) {
+	mockPort := utils.Must(test.GetFreePort())
+
+	conn := NewConnection(Options{})
+	assert.NotNil(t, conn)
+	err := conn.Connect("localhost:" + strconv.Itoa(mockPort))
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "failed to connect")
+}
+
+func TestSearch(t *testing.T) {
+	conn := &Connection{}
+	_, err := conn.Search("@attr 1=4 utah")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "connection is not established")
+
+	options := Options{
+		"preferredRecordSyntax": "usmarc",
+	}
+
+	conn = NewConnection(options)
+	assert.NotNil(t, conn)
+	err = conn.Connect("z3950.indexdata.com/marc")
+	assert.NoError(t, err)
+
+	_, err = conn.Search("@attr 1=99 program")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "search failed: Unsupported Use attribute")
+
+	rs, err := conn.Search("@attr 1=4 computer")
+	assert.NoError(t, err)
+	assert.NotNil(t, rs)
+	assert.Greater(t, rs.Count(), 7)
+
+	rs, err = conn.Search("@attr 1=4 program")
+	assert.NoError(t, err)
+	assert.NotNil(t, rs)
+	assert.Greater(t, rs.Count(), 2)
+
+	record, err := rs.GetRecord(0)
+	assert.NoError(t, err)
+	assert.NotNil(t, record)
+	assert.Contains(t, record.Data("render"), "program")
+}
