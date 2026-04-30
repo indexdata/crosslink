35df9bfe25fff818e21d4f652e309bc3142ea20e
Jakub Skoczen <jakub@indexdata.com>
2026-04-30 10:52:29 +0200

    Calculate notification count once

--- broker/sqlc/pr_schema.sql
+++ broker/sqlc/pr_schema.sql
@@ -78,17 +78,14 @@ SELECT
         FROM notification n
         WHERE n.pr_id = pr.id and cost is not null
     ) AS has_cost,
-    EXISTS (
-        SELECT 1
-        FROM notification n
-        WHERE n.pr_id = pr.id and acknowledged_at is null
-    ) AS has_unread_notification,
+    (unread.unread_notifications_count > 0) AS has_unread_notification,
     pr.ill_request -> 'serviceInfo' ->> 'serviceType' AS service_type,
     pr.ill_request -> 'serviceInfo' -> 'serviceLevel' ->> '#text' AS service_level,
     immutable_to_timestamp(pr.ill_request -> 'serviceInfo' ->> 'needBeforeDate') AS needed_at,
-    (
-        SELECT COUNT(*)
-        FROM notification n
-        WHERE n.pr_id = pr.id and acknowledged_at is null
-    ) AS unread_notifications_count
-FROM patron_request pr;
+    unread.unread_notifications_count AS unread_notifications_count
+FROM patron_request pr
+LEFT JOIN LATERAL (
+    SELECT COUNT(*) AS unread_notifications_count
+    FROM notification n
+    WHERE n.pr_id = pr.id and n.acknowledged_at is null
+) unread ON true;
