e206b14bdc7cabf3ba18cbcd021b2025f85e034a
Jakub Skoczen <jakub@indexdata.com>
2026-05-29 00:30:34 +0200

    Add sentinels for codegen deps

---
+++ directory/codegen_deps.go
@@ -0,0 +1,7 @@
+package directory
+
+import (
+	// Keep dependencies used only by generated OpenAPI code in go.mod.
+	_ "github.com/oapi-codegen/runtime"
+	_ "github.com/oapi-codegen/runtime/types"
+)
