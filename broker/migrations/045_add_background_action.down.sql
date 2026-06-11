52943c50f1929b67a8757a7de7f0643ba99c7145
Janis Saldabols <janis@indexdata.com>
2026-06-11 08:54:03 +0300

    CROSSLINK-265 Fix script

--- broker/migrations/045_add_background_action.down.sql
+++ broker/migrations/045_add_background_action.down.sql
@@ -2,4 +2,4 @@ DELETE FROM event WHERE event_name = 'invoke-background-action';
 DELETE FROM event_config WHERE event_name = 'invoke-background-action';
 
 ALTER TABLE scheduled_task
-    DROP COLUMN title;
+    DROP COLUMN IF EXISTS title;
