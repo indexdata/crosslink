2b5daadcd06b50d69e6e29e235628f11299c777e
Adam Dickmeiss <adam@indexdata.dk>
2026-06-24 13:58:03 +0200

    CP2

--- illmock/app/requester.go
+++ illmock/app/requester.go
@@ -77,10 +77,7 @@ func (app *MockApp) handlePatronRequest(illMessage *iso18626.ISO18626Message, w 
 	header := &msg.Request.Header
 
 	// ServiceInfo != nil already
-	note := ""
-	if illRequest.ServiceInfo != nil {
-		note = illRequest.ServiceInfo.Note
-	}
+	note := illRequest.ServiceInfo.Note
 	cancel := note == "#CANCEL#"
 	renew := note == "#RENEW#"
 	retryKeepId := strings.Contains(note, "#RETRYKEEPID#")
