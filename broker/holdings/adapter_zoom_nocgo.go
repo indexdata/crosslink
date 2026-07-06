5ec95a7acb217b1b032751ce1990b3d614e06b90
Adam Dickmeiss <adam@indexdata.dk>
2026-07-06 13:39:06 +0200

    CP

--- broker/holdings/adapter_zoom_nocgo.go
+++ broker/holdings/adapter_zoom_nocgo.go
@@ -12,6 +12,6 @@ func cgoEnabled() bool { return false }
 
 type ZoomAvailabilityAdapter struct{}
 
-func NewZoomAvailabilityAdapter(config directory.ZoomConfig, queryBuilder LookupQueryBuilder, holdingsParser HoldingsParser) (LookupAdapter, error) {
+func NewZoomAvailabilityAdapter(config directory.ZoomConfig, queryBuilder LookupQueryBuilder, holdingsParser HoldingsParser, metadataParser MetadataParser) (LookupAdapter, error) {
 	return nil, fmt.Errorf("ZOOM availability adapter requires cgo, but cgo is not enabled")
 }
