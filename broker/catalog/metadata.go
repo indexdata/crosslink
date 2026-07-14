ff580d3c0a3e643292171eed04a588bb108f67c3
Adam Dickmeiss <adam@indexdata.dk>
2026-07-14 11:24:26 +0200

    Move LookupParamsFromBibliographicInfo to metadata.go

--- broker/catalog/metadata.go
+++ broker/catalog/metadata.go
@@ -1,6 +1,8 @@
 package catalog
 
 import (
+	"strings"
+
 	"github.com/indexdata/crosslink/directory"
 	"github.com/indexdata/crosslink/iso18626"
 )
@@ -52,4 +54,25 @@ func MetadataRequestUpdate(illRequest *iso18626.BibliographicInfo, metadata Meta
 	fixupBibliographicItem(&illRequest.BibliographicItemId, "ISBN", metadata.Isbn, replace)
 	fixupBibliographicItem(&illRequest.BibliographicItemId, "ISSN", metadata.Issn, replace)
 	return nil
+}
+
+func LookupParamsFromBibliographicInfo(info iso18626.BibliographicInfo, serviceInfo *iso18626.ServiceInfo) LookupParams {
+	var serviceType string
+	if serviceInfo != nil {
+		serviceType = string(serviceInfo.ServiceType)
+	}
+	params := LookupParams{
+		Identifier:  info.SupplierUniqueRecordId,
+		Title:       info.Title,
+		ServiceType: serviceType,
+	}
+	for _, id := range info.BibliographicItemId {
+		switch strings.TrimSpace(id.BibliographicItemIdentifierCode.Text) {
+		case "ISBN":
+			params.Isbn = id.BibliographicItemIdentifier
+		case "ISSN":
+			params.Issn = id.BibliographicItemIdentifier
+		}
+	}
+	return params
 }
