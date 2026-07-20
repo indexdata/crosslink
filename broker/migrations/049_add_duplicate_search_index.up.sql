0cdda4db0253484c0d3a2da8edb6b6174a8451e1
JanisSaldabols <143704574+JanisSaldabols@users.noreply.github.com>
2026-07-20 12:29:30 +0300

    CROSSLINK-266 Check incoming duplication messages (#660)
    * CROSSLINK-266 Check incoming duplication messages
    
    * CROSSLINK-266 Add duplicate action outcome
    
    * CROSSLINK-266 Fix migration script
    
    * CROSSLINK-266 Fix copilot comments
    
    * CROSSLINK-266 Add duplicate test
    
    * CROSSLINK-266 Use CQL to search duplicates
    
    * CROSSLINK-266 Use exact match for title and patron id

---
+++ broker/migrations/049_add_duplicate_search_index.up.sql
@@ -0,0 +1,23 @@
+CREATE INDEX IF NOT EXISTS idx_ill_transaction_requester_timestamp
+    ON ill_transaction (requester_symbol, timestamp)
+    INCLUDE (id);
+
+CREATE INDEX IF NOT EXISTS idx_ill_transaction_isbn
+    ON ill_transaction
+    USING gin (bibliographic_item_identifiers(ill_transaction_data, 'ISBN'));
+
+CREATE INDEX IF NOT EXISTS idx_ill_transaction_issn
+    ON ill_transaction
+    USING gin (bibliographic_item_identifiers(ill_transaction_data, 'ISSN'));
+
+CREATE INDEX idx_ill_transaction_title_tsv
+    ON ill_transaction ((ill_transaction_data->'bibliographicInfo'->>'title'));
+
+CREATE INDEX IF NOT EXISTS idx_ill_transaction_supplier_unique_record_id
+    ON ill_transaction ((ill_transaction_data->'bibliographicInfo'->>'supplierUniqueRecordId'));
+
+CREATE INDEX IF NOT EXISTS idx_ill_transaction_patron_id
+    ON ill_transaction ((ill_transaction_data->'patronInfo'->>'patronId'));
+
+CREATE INDEX IF NOT EXISTS idx_ill_transaction_service_type
+    ON ill_transaction ((ill_transaction_data->'serviceInfo'->>'serviceType'));
