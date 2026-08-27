c5a736930f5dd9020b8b50912793dae02549b698
Jakub Skoczen <jakub@indexdata.com>
2026-08-28 00:21:04 +0200

    CoPilot

--- broker/migrations/059_add_ill_message_events.down.sql
+++ broker/migrations/059_add_ill_message_events.down.sql
@@ -1,2 +1,5 @@
+DELETE FROM event
+WHERE event_name IN ('ill-requester-message', 'ill-supplier-message');
+
 DELETE FROM event_config
 WHERE event_name IN ('ill-requester-message', 'ill-supplier-message');
