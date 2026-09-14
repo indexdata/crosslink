e8990edbd3d95a35b44064ee0dc819d898c57392
Adam Dickmeiss <adam@indexdata.dk>
2026-09-14 19:19:27 +0200

    return first shipping address

--- broker/common/directory_address.go
+++ broker/common/directory_address.go
@@ -5,31 +5,34 @@ import (
 	"github.com/indexdata/crosslink/iso18626"
 )
 
-// DirectoryShippingAddress converts the directory shipping address to ISO18626.
+// DirectoryShippingAddress converts the first Shipping address with non-nil AddressComponents to ISO18626.
+// An empty component list returns an empty address; later addresses are ignored.
 func DirectoryShippingAddress(entry dirapi.Entry) iso18626.PhysicalAddress {
 	address := iso18626.PhysicalAddress{}
 	if entry.Addresses != nil {
 		for _, addr := range *entry.Addresses {
-			if addr.AddressComponents != nil && addr.Type == "Shipping" {
-				for _, comp := range *addr.AddressComponents {
-					switch comp.Type {
-					case "Thoroughfare":
-						address.Line1 = comp.Value
-					case "Locality":
-						address.Locality = comp.Value
-					case "AdministrativeArea":
-						address.Region = &iso18626.TypeSchemeValuePair{
-							Text: comp.Value,
-						}
-					case "PostalCode":
-						address.PostalCode = comp.Value
-					case "CountryCode":
-						address.Country = &iso18626.TypeSchemeValuePair{
-							Text: comp.Value,
-						}
+			if addr.Type != "Shipping" || addr.AddressComponents == nil {
+				continue
+			}
+			for _, comp := range *addr.AddressComponents {
+				switch comp.Type {
+				case "Thoroughfare":
+					address.Line1 = comp.Value
+				case "Locality":
+					address.Locality = comp.Value
+				case "AdministrativeArea":
+					address.Region = &iso18626.TypeSchemeValuePair{
+						Text: comp.Value,
+					}
+				case "PostalCode":
+					address.PostalCode = comp.Value
+				case "CountryCode":
+					address.Country = &iso18626.TypeSchemeValuePair{
+						Text: comp.Value,
 					}
 				}
 			}
+			return address
 		}
 	}
 	return address
